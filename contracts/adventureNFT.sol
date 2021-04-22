e1973f0572449561ea9576e14c2dc1c9b31c8709
Altaergo <73134512+Altaergo@users.noreply.github.com>
2021-04-22 20:28:08 +0300

    update baseuri function

--- contracts/adventureNFT.sol
+++ contracts/adventureNFT.sol
@@ -12,7 +12,7 @@ contract adventureNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    //token payments
+    //users deposit tokens to be used to mint NFTs
     mapping(address => uint256) public playerAmounts;
 
     // this marks an item in IPFS as "forsale"
@@ -20,28 +20,35 @@ contract adventureNFT is ERC721URIStorage {
     mapping(bytes32 => uint256) public uriToTokenId;
 
     constructor(bytes32[] memory assetsForSale) ERC721("adventureNFT", "ANFT") {
-        _baseURI("https://ipfs.io/ipfs/");
+        _baseURI();
         for (uint256 i = 0; i < assetsForSale.length; i++) {
             forSale[assetsForSale[i]] = true;
         }
     }
 
-    function transferTokens(uint256 amount, address _nipToken) public {
+function _baseURI() override internal view virtual returns (string memory) {
+        return "https://ipfs.io/ipfs/";
+    }
+    function transferTokens(uint256 amount, address _nipToken)
+        public
+        returns (uint256)
+    {
         require(
             IERC20(_nipToken).transferFrom(msg.sender, address(this), amount),
             "TRANSFER FAILED"
         );
         playerAmounts[msg.sender] += amount;
+        return playerAmounts[msg.sender];
     }
 
     function createAdventureNFT(string memory tokenURI)
         public
         returns (uint256)
     {
-        require(playerAmounts[msg.sender] >= 10000, "INADEQUATE TOKENS");
         bytes32 uriHash = keccak256(abi.encodePacked(tokenURI));
         //make sure they are only minting something that is marked "forsale"
         require(forSale[uriHash], "NOT FOR SALE");
+        require(playerAmounts[msg.sender] >= 10000, "INADEQUATE TOKENS");
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
@@ -53,42 +60,3 @@ contract adventureNFT is ERC721URIStorage {
         return newItemId;
     }
 }
-
-// contract YourCollectible is ERC721 {
-
-//   using Counters for Counters.Counter;
-//   Counters.Counter private _tokenIds;
-
-//   constructor(bytes32[] memory assetsForSale) public ERC721("YourCollectible", "YCB") {
-//     _setBaseURI("https://ipfs.io/ipfs/");
-//     for(uint256 i=0;i<assetsForSale.length;i++){
-//       forSale[assetsForSale[i]] = true;
-//     }
-//   }
-
-//   //this marks an item in IPFS as "forsale"
-//   mapping (bytes32 => bool) public forSale;
-//   //this lets you look up a token by the uri (assuming there is only one of each uri for now)
-//   mapping (bytes32 => uint256) public uriToTokenId;
-
-//   function mintItem(string memory tokenURI)
-//       public
-//       returns (uint256)
-//   {
-//       bytes32 uriHash = keccak256(abi.encodePacked(tokenURI));
-
-//       //make sure they are only minting something that is marked "forsale"
-//       require(forSale[uriHash],"NOT FOR SALE");
-//       forSale[uriHash]=false;
-
-//       _tokenIds.increment();
-
-//       uint256 id = _tokenIds.current();
-//       _mint(msg.sender, id);
-//       _setTokenURI(id, tokenURI);
-
-//       uriToTokenId[uriHash] = id;
-
-//       return id;
-//   }
-// }
